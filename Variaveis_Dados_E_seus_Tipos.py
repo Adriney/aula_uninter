280fed0b29de31cbb73e903da4e9778fef3360c4
Adriney <142994789+Adriney@users.noreply.github.com>
2024-05-02 20:06:34 -0300

    Create Variaveis_Dados_E_seus_Tipos.py

---
+++ Variaveis_Dados_E_seus_Tipos.py
@@ -0,0 +1,8 @@
+#Variáveis, dados e seus tipos
+nota = 8.5
+disciplina = 'Lógica de Programação e Algoritmos'
+print(nota)
+print(disciplina)
+
+print("="*30)
+print('Dsciplina: ',disciplina,'Nota:', nota)
