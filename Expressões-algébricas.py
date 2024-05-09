2601863bc643391344c184faaa7822f287f3a035
Adriney <142994789+Adriney@users.noreply.github.com>
2024-05-09 14:21:33 -0300

    Create Expressões-algébricas.py

---
+++ Expressões-algébricas.py
@@ -0,0 +1,28 @@
+print(1 + 2 + 3 + 4 + 5)
+#somatório dos 5 primeiros números inteiros e positivos
+print('--'*15)
+
+print((23 + 19 + 31) / 3)
+# A média entre 23,19 e 31
+print('--'*15)
+
+print(403 // 73)# // divisao da parte inteira
+# o número de vezes que o 73 cabe em 403
+print('--'*15)
+
+print(403 % 73)
+# A sobra quando 403 é dividido por 73
+print('--'*15)
+
+print(2 ** 10)
+# 2 elevado à 10ª potência
+print('--'*15)
+
+print(abs(54 - 57))
+#O valor absoluto da diferença entre 54 e 57
+print('--'*15)
+
+print(min(34, 29, 31))
+#O menor valor entre 34,29 e 31
+print('--'*15)
+
