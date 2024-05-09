f96243be083f247b36be35a74119a3e95ea4704a
Adriney <142994789+Adriney@users.noreply.github.com>
2024-05-09 14:21:05 -0300

    Create Atribuição.py

---
+++ Atribuição.py
@@ -0,0 +1,8 @@
+# Escreva as espressões em Python para:
+#a)Atribuir o valor inteiro 3 à variável a
+a = 3
+#b) Atribuir o valor 4 à variável b
+b = 4
+#c) Atribuir à variável c o valor da expressão a*a + b * b
+c = a * a + b * b
+print(c)
