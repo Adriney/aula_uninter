c98b69e06fe9c3d14a995c4d7fcac47666c456e2
Adriney <142994789+Adriney@users.noreply.github.com>
2024-05-13 13:30:30 -0300

    Create Expressões-boleanas2.py

---
+++ Expressões-boleanas2.py
@@ -0,0 +1,17 @@
+#Escreva as seguintes expressões booleanas em linguagem Python
+#a) 1387 édivisivel por 19
+if(1387 % 19 == 0):
+    print('Verdadeiro!')
+else:
+    print('Falso!')
+#b)31 é par
+if(31 % 2 == 0):
+    print("Verdadeiro")
+else:
+    print('Falso!')
+
+#c)O menor valor entre: 34, 29 e 31 é menor que 30
+if(min(34, 29, 31) < 30):
+    print('Verdadeiro!')
+else:
+    print('Falso!')
