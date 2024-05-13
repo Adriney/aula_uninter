6a6db4e3cd138b61f43cfdba4b755227dc96b187
Adriney <142994789+Adriney@users.noreply.github.com>
2024-05-13 13:31:51 -0300

    Create ex007(001).py

---
+++ ex007(001).py
@@ -0,0 +1,4 @@
+#Faça um algoritmo que receba três valores, representando os lados de um triângulo fornecidos pelos usuário. Verifique se os valores formam um triânulo e clasifique como
+#a)Equilátero(três lados iguais)
+#b)isóceles (dois lados iguais)
+#c)Escaleno (três lados diferentes)
